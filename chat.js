2418f521d205fb1b3f217b7fff5b3271e8ead273
abironVijay <137709502+abironVijay@users.noreply.github.com>
2023-10-24 21:01:54 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,23 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBJb7J6R7jYJKeAEgFhEboqYNSUy5RNBfQ",
+    authDomain: "letschatwebapp-ae56e.firebaseapp.com",
+    projectId: "letschatwebapp-ae56e",
+    storageBucket: "letschatwebapp-ae56e.appspot.com",
+    messagingSenderId: "1093146449732",
+    appId: "1:1093146449732:web:27a9b1da60633ad11c2911"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  function addUser() {
+    user_name=document.getElementById("user_name").value;
+    localStorage.setItem("user_name",user_name);
+    window.location="chatroom.html";
+}
+
+
+
